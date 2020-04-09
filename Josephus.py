66498360606cd97b9cf73dd51950c83533456a96
kriswangz <wangzhen1996me@163.com>
2020-04-09 16:02:01 +0800

    use dictionary, including name and age to judge who is the survivor

--- Josephus.py
+++ Josephus.py
@@ -1,3 +1,4 @@
+# %%
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -10,32 +11,52 @@
 #           Counting from the first person, when count to the step value, he must commit suicide, 
 #           and then report again from the next, until everyone suicide.
 # Author: Chris Wang
+
 class Josephus:
-    def __init__(self, name, step, start):
-        self.name = name
+    """
+    input rings([0,1..n]), this class will output the survivor index numbers in rings.
+    """
+    def __init__(self, rings, step, start):
+        self.rings = rings
         self.step = step
         self.start = start
-    
+
     def judge_survive(self):
+        p = self.start       # index point
+
+        for i in range(len(self.rings)):
+            if len(self.rings) == 1:
+                break
+
+            p = (p + (self.step - 1)) % len(self.rings)
+            self.rings.pop(p)
+
+        return self.rings[0]     #return survive people's index.
+
+class  CreateRings:
+    """
+    convert name items to rings, use numbers instead of complex characters like dictionary
+    """
+    def __init__(self, name):
+        self.name = name
+    
+    def create_rings(self):
         length = int(len(self.name))
 
         if length == 1:
-            return self.name[0]
+            return [0]
+        
+        self.rings = [i for i in range(length)]
 
-        p = self.start       # index point
-        for i in range(length):
-            if len(self.name) == 1:
-                break
+        return self.rings
 
-            p = (p + (self.step - 1)) % len(self.name)
-            self.name.pop(p)
-
-        return self.name[0]     #return survive people's index.
-
-name = ["Chris", "Anna", "Bob", "David"]
-step = 1
+name = {0:['Chris','24'], 1:['Anna','18'], 3:["Bob",'30'], 4:["David",'21']}
+step = 2
 start = 0
 
-joseph = Josephus(name, step, start)
-survive = joseph.judge_survive()
-print("survive:",survive)
+rings = CreateRings(name)
+ring = rings.create_rings()  # generate rings [0..n] n=len(name)
+
+joseph = Josephus(ring, step, start)
+survive = joseph.judge_survive() # return index value in rings
+print("survive is %s, age is %s" %(name[survive][0],name[survive][1]))
