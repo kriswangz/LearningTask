67d8c45509938f6ac3d83e68cef945db95dd5315
kriswangz <wangzhen1996me@163.com>
2020-04-09 10:26:39 +0800

    change naming

--- Josephus.py
+++ Josephus.py
@@ -3,7 +3,7 @@
 
 # The program is used for solving josephus promblem.
 # character:
-#           total: total chars, names included.
+#           name: total characters, names included.
 #           step: step value.
 #           start: start point.
 # description:
@@ -11,31 +11,31 @@
 #           and then report again from the next, until everyone suicide.
 # Author: Chris Wang
 class Josephus:
-    def __init__(self, total, step, start):
-        self.total = total
+    def __init__(self, name, step, start):
+        self.name = name
         self.step = step
         self.start = start
     
     def judge_survive(self):
-        n = int(len(self.total))
+        length = int(len(self.name))
 
-        if n == 1:
-            return self.total[0]
+        if length == 1:
+            return self.name[0]
 
         p = self.start       # index point
-        for i in range(n):
-            if len(self.total) == 1:
+        for i in range(length):
+            if len(self.name) == 1:
                 break
 
-            p = (p + (self.step - 1)) % len(self.total)
-            self.total.pop(p)
+            p = (p + (self.step - 1)) % len(self.name)
+            self.name.pop(p)
 
-        return self.total[0]     #return survive people's index.
+        return self.name[0]     #return survive people's index.
 
-total = ["Chris", "Anna", "Bob", "David"]
+name = ["Chris", "Anna", "Bob", "David"]
 step = 1
 start = 0
 
-joseph = Josephus(total, step, start)
+joseph = Josephus(name, step, start)
 survive = joseph.judge_survive()
 print("survive:",survive)
