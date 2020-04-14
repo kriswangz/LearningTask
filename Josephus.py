d28c11a0535928e0c190fb9b2a878dc6c20f01e3
kriswangz <wangzhen1996me@163.com>
2020-04-14 19:26:07 +0800

    change class ways of create rings into def

--- Josephus.py
+++ Josephus.py
@@ -4,9 +4,10 @@
 
 # The program is used for solving josephus promblem.
 # description:
-#           Counting from the first person, when count to the step value, he must commit suicide, 
+#           Counting from the first person, when count to the step value, he must commit suicide,
 #           and then report again from the next, until the last one.
 # Author: Chris Wang
+
 
 class Josephus:
     """
@@ -20,6 +21,7 @@ class Josephus:
     Returns:
         judge_survivor: return survive people's index in a ring like 0(rings[0]).
     """
+
     def __init__(self, rings, step, start):
         self.rings = rings
         self.step = step
@@ -35,9 +37,10 @@ class Josephus:
             p = (p + (self.step - 1)) % len(self.rings)
             self.rings.pop(p)
 
-        return self.rings[0]    
+        return self.rings[0]
 
-class  CreateRings:
+
+def create_rings(name):
     """
     convert name items to rings, use numbers instead of complex characters like dictionary.
 
@@ -47,31 +50,29 @@ class  CreateRings:
     Return:
         rings: from input items to rings[0..n]
     """
-    def __init__(self, name):
-        self.name = name
-    
-    def create_rings(self):
-        length = int(len(self.name))
+    length = int(len(name))
 
-        if length == 1:
-            return [0]
-        
-        self.rings = [i for i in range(length)]
+    if length == 1:
+        return [0]
 
-        return self.rings
-        
+    rings = [i for i in range(length)]
+
+    return rings
+
+
 if __name__ == '__main__':
-    name = {0:['Chris','24','male'], 1:['Anna','18','female'], 3:["Bob",'30','male'], 4:["David",'21','male']}
+    name = {0: ['Chris', '24', 'male'], 1: ['Anna', '18', 'female'],
+            3: ["Bob", '30', 'male'], 4: ["David", '21', 'male']}
     step = 2
     start = 0
-    name_offset = 0 # offset index in return list ['name', 'age', 'gender']
+    name_offset = 0  # offset index in return list ['name', 'age', 'gender']
     age_offset = 1
     gender_offset = 2
 
-    ring = CreateRings(name)
-    rings = ring.create_rings()  # generate rings [0..n] n=len(name)
+    rings = create_rings(name)  # generate rings [0..n] n=len(name)
 
     joseph = Josephus(rings, step, start)
-    survivor = joseph.judge_survivor() # return index value in rings, ['name', 'age',  'gender'] included.
-    print("Survivor's name is %s, age is %s, gender is %s" 
-            % (name[survivor][name_offset],name[survivor][age_offset],name[survivor][gender_offset]))
+    # return index value in rings, ['name', 'age',  'gender'] included.
+    survivor = joseph.judge_survivor()
+    print("Survivor's name is %s, age is %s, gender is %s"
+          % (name[survivor][name_offset], name[survivor][age_offset], name[survivor][gender_offset]))
