e04478410544ba267a9465267a25c77cd06b3ac0
kriwangz <wangzhen1996me@163.com>
2020-04-29 09:41:02 +0800

    删除read file文件并把内容移到Josephus.py中

--- Josephus.py
+++ Josephus.py
@@ -1,7 +1,7 @@
 '''
 @Author: Chris Wang
 @Date: 2020-04-27 01:59:21
-@LastEditTime: 2020-04-28 16:16:14
+@LastEditTime: 2020-04-29 09:39:50
 @LastEditors: Please set LastEditors
 @Description: Solve Josephus problem. Counting from the first person, 
             when count to the step value, he must commit suicide,
@@ -19,7 +19,6 @@ import copy
 import sys
 import read_file
 import csv
-
 '''
 @description: return object, one object indicates one person,
             included name, age and gender.
@@ -153,6 +152,92 @@ class Ring(object):
 
         return obj
 
+class Read_file(object):
+    """
+    interface class, for smooth reading.
+    """
+
+    def read(self, path, filename, mode='r'):
+        raise NotImplementedError
+
+
+def str2list_row(row):
+    """
+    convert str to list in a row.
+    """
+    row = row.strip('\n')   # delete '\n'.
+    row = row.strip('\r')   # delete '\r'.
+    row = row.split(',')
+
+    return row
+
+
+class Read_csv(Read_file):
+
+    def read(self, path, filename, mode='r'):
+        cache = []
+        with open(path + '/' + filename, mode) as csvfile:
+            read_csv = csv.reader(csvfile, delimiter=',')
+
+            for row in read_csv:
+                cache.append(row)
+
+        return cache
+
+
+class Read_txt(Read_file):
+
+    def read(self, path, filename, mode='r'):
+        cache = []
+        with open(path + '/' + filename, mode) as fp:
+            read_txt = fp.readlines()
+
+            for row in read_txt:
+                row = str2list_row(row)
+                cache.append(row)
+
+        return cache
+
+
+class Read_zip(Read_file):
+    """
+    only read .csv and .txt files function in zip files is supported.
+    if u wanna support more file types, please add your program in 
+    this class.
+    """
+
+    def read(self, path, filename, mode='r'):
+        cache = []
+        with zipfile.ZipFile(path, mode) as z:
+
+            namelist = z.namelist()
+            if filename not in namelist:
+                raise FileNotFoundError
+
+            # get files suffix and judge.
+            filename_split = filename.split('.')
+            split_len = len(filename_split)
+            filename_suffix = filename_split[split_len - 1]
+
+            if filename_suffix == 'txt' or 'csv':
+                fp = z.open(filename)
+                read_txt = fp.readlines()
+                fp.close()
+
+                for row in read_txt:
+                    row = bytes.decode(row)
+                    row = str2list_row(row)
+                    cache.append(row)
+            else:
+                raise FileExistsError
+
+        return cache
+
+
+def read_data(file_obj, path, filename, mode):
+    cache = []
+    cache = file_obj.read(path, filename, mode)
+    return cache
 
 """
     The format of each object should correspond to the parameters,
